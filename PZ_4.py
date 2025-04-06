3e10027af05941e61a106fc11fa014189a1a1683
DianaVolosuk214 <dianavolosuk214@gmail.com>
2025-04-06 13:54:19 +0300

    Update PZ_4.py

--- PZ_4.py
+++ PZ_4.py
@@ -1,1 +1,7 @@
 
+price = float(input(":"))
+current_weight = 1.2
+while current_weight <= 2.0:
+  total_cost = price * current_weight
+  print(f"{current_weight} : {total_cost:.2f}
+  current_weight += 0.2
