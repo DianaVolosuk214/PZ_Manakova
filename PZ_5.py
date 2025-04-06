85fe6b88b9eecb63935101b9ad839a0d8870689e
DianaVolosuk214 <dianavolosuk214@gmail.com>
2025-04-06 14:57:18 +0300

    Update PZ_5.py

--- PZ_5.py
+++ PZ_5.py
@@ -1,1 +1,21 @@
+# Делала Волощук Диана из Ис-25
+# Задание 1
 
+# Задание 2
+def DigitCountSum(K):
+  C = 0 # Count Количество
+  S = 0 # Sum Сумма
+  num = K
+
+  while num > 0:
+    digit = num % 10
+    C += 1
+    S += digit
+    num = num // 10
+  return C, S
+  
+numbers = [int(input(f"Введите число {i+1}: ")) for i in range(5)]
+
+for num in numbers:
+  count, sum_digits = DigitCountSum(num)
+  print(f"Число {num}: количество цифр = {count}, сумма цифр = {sum_digits}") 
