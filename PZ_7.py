51e575c93f637d0e1c8053bff5453c8fa4aa6fb1
DianaVolosuk214 <dianavolosuk214@gmail.com>
2025-04-12 14:18:05 +0300

    Update PZ_7.py

--- PZ_7.py
+++ PZ_7.py
@@ -1,1 +1,11 @@
+# Делала Волощук Диана из Ис-25
+# Задание 1
+def reverse(N):
+  if 1 < N < 26:
+    last = [chr(ord('z') - i ) for i in range(N)]
+    print(' '.join(last))
+  else:
+    print("N должна быть в диапазоне 1 < N < 26")
 
+N = int(input("")
+reverse(N)
