5ef5c842828d7684898ab8771afea9d2442d31a7
DianaVolosuk214 <dianavolosuk214@gmail.com>
2025-04-12 13:56:04 +0300

    Update PZ_6.py

--- PZ_6.py
+++ PZ_6.py
@@ -1,1 +1,11 @@
+# Делала Волощук Диана из Ис-25
+# Задание №6 задача №1
+def gen_fib_num():
+  N = 10 
 
+  fib = [1, 1]
+  for i in range(2, N):
+    fib.append(fib[i-2] + fib[i-1])
+  return fib
+
+print (gen_fib_num())
