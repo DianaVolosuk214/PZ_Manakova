3b26a83310aa263aa18174b57becb991e2f1afe4
DianaVolosuk214 <dianavolosuk214@gmail.com>
2025-04-12 14:45:41 +0300

    Update PZ_7.py

--- PZ_7.py
+++ PZ_7.py
@@ -6,6 +6,36 @@ def reverse(N):
     print(' '.join(last))
   else:
     print("N должна быть в диапазоне 1 < N < 26")
-
-N = int(input("")
+N = int(input("Введите N:"))
 reverse(N)
+
+# Задание 2
+def enc_sent(sentence, K):
+  if not (0 < K < 10):
+    return "К должна быть в диапазоне от 0 до 10 (не включительно)"
+
+  encrypted = []
+  for char in sentense:
+    if char.isalpha():
+      if char.islower():
+        base = ord("а")
+        end = ord("я")
+      else:
+        base = ord("А")
+        end = ord("Я")
+      if char.lower() == "ё":
+        encrypted.append(char)
+        continue
+      shifted = ord(char) + K
+      if shifted > end:
+        shifted -=32
+      encrypted.append(chr(shifted))
+    else:
+      encrypted.append(char)
+  return " ".join(encrypted)
+sentence = input(":")
+K = int(input(":"))
+print(enc_sent(sentence, K))
+      
+        
+        
