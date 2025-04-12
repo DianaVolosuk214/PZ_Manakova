8f6302bc8f02575d750fe753db062afdf95e7457
DianaVolosuk214 <dianavolosuk214@gmail.com>
2025-04-12 14:08:49 +0300

    Update PZ_6.py

--- PZ_6.py
+++ PZ_6.py
@@ -9,3 +9,13 @@ def gen_fib_num():
   return fib
 
 print (gen_fib_num())
+
+# Задание №6 задача №2
+def reverse(A, K, L):
+  if 1 < K < L < len(A):
+    A[K-1:L] = A[K-1:L][::-1]
+  return A
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+K = int(input("Введите диапазон K:")
+L = int(input("Введите диапазон L:")
+print(reverse(A, K, L)
