23f4f35492cd4869b97b1637dcacccd42217f7d9
DianaVolosuk214 <dianavolosuk214@gmail.com>
2025-04-27 15:58:50 +0300

    Update PZ_6.py

--- PZ_6.py
+++ PZ_6.py
@@ -16,6 +16,29 @@ def reverse(A, K, L):
     A[K-1:L] = A[K-1:L][::-1]
   return A
 A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-K = int(input("Введите диапазон K:")
-L = int(input("Введите диапазон L:")
-print(reverse(A, K, L)
+K = int(input("Введите диапазон K:"))
+L = int(input("Введите диапазон L:"))
+print(reverse(A, K, L))
+
+# Задание №6 задача №3
+def remove_local_maximum(arr): # функция, которая удаляет локальные максимумы из списка arr
+  if not arr:
+    return arr # если список пустой, то возвращаем пустой список
+  n = len(arr)
+  result = arr.copy()
+  
+  for i in range(n): # проверяем каждый элемент списка
+    is_local_maximum = True
+    if i > 0 and arr[i] <= arr[i-1]: # проверяем, является ли элемент локальным максимумом
+      is_local_maximum = False # не трогаем элемент, если он не максимум      
+    if i < n - 1 and arr [i] <= arr[i+1]: # проверяем, является ли элемент локальным максимумом
+       is_local_maximum = False # не трогаем элемент, если он не максимум
+      
+    if is_local_maximum: # если элемент локальный максумум, то удаляем его из списка
+      result[i] = 0 
+  return result # возвращаем список без локальных максимумов
+  
+N = int(input("размер списка: "))
+arr = list(map(int, input("введите элементы спска через пробел: ").split()))
+print("исходный список: ", arr)
+print("список после удаления локальных максимумов: ", remove_local_maximum(arr))
