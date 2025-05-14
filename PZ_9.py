9af0412b85d33e9f6aa0f123e8f14e274a20f343
DianaVolosuk214 <dianavolosuk214@gmail.com>
2025-05-14 17:29:34 +0300

    Update PZ_9.py

--- PZ_9.py
+++ PZ_9.py
@@ -1,1 +1,17 @@
+fruits = {
+  "цвет":"красный",
+  "форма":"круглая",
+  "вкус":"кислый"
+}
+print(f"первоначальный словарь: {fruits}")
 
+found = False
+
+for key, value in fruits.items():
+  if key == "фрукт" and value == "яблоко":
+    found = True
+    break
+
+if not found:
+  fruits["фрукт"] = "яблоко"
+  print(f":{fruits}")
